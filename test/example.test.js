68f67ca99e3a2418d26de8de9b96b769e8f1216e
Sofia Tejada <sofianais@icloud.com>
2021-04-29 18:59:27 -0700

    add TDD test for image change function

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { setCharge, setMood, setPoki } from '../functions.js';
+import { setCharge, setMood, setPoki, setImage } from '../functions.js';
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
@@ -8,17 +8,21 @@ test('time to test a function', (expect) => {
     // Set up your arguments and expectations
     const charge = 'low';
     const mood = 'low';
-    const poki = 'bulbasaur';
+    const poki = 'charizard';
+    const image = 'charizard image';
     
     //Act 
     // Call the function you're testing and set the result to a const
     const actualCharge = setCharge(3);
     const actualMood = setMood(3);
     const actualPoki = setPoki(3);
+    const actualImage = setImage('charizard');
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actualCharge, charge);
     expect.equal(actualMood, mood);
     expect.equal(actualPoki, poki);
+    expect.equal(actualImage, image);
 });
+
